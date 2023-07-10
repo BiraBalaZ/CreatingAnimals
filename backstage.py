15a46c8a1630053f44bbe7e378b5c54509f595a6
Erick <erick.m.anjos@gmail.com>
2023-07-10 18:20:40 -0300

    Fixing the code

--- backstage.py
+++ backstage.py
@@ -1,15 +1,16 @@
 class CreateAnimal:
-    def __init__(self, size, temperament, type, legs='Undefined', description ='Undefined'):
+    def __init__(self, size, temperament, type, legs='Undefined', description ='Nothing to declare'):
 
-        # Iniciando um animal        
+        # Iniciando um animal
         self.size = size
         self.temperament = temperament
         self.type = type
         self.legs = legs
         self.description = description
 
+    # Criando um animal de fato
     def generatingAnimal(self):
-        if (self.legs != 'Undefined' or self.legs == 0):
-            return f'O Animal em questão tem {self.legs} patas, tem temperamento "{self.temperament}", tem tamanho "{self.size}" e é do tipo "{self.type}"'
+        if (self.legs != 'Undefined'):
+            return f'O Animal em questão tem {self.legs} patas, tem temperamento "{self.temperament}", tem tamanho "{self.size}" e é do tipo "{self.type}"\nDescrição: {self.description}\n'
         else:
-            return f'O Animal em questão não possui patas, tem temperamento "{self.temperament}", tem tamanho "{self.size}" e é do tipo "{self.type}"'
+            return f'O Animal em questão não possui patas, tem temperamento "{self.temperament}", tem tamanho "{self.size}" e é do tipo "{self.type}"\nDescrição: {self.description}\n'
--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ dog = CreateAnimal('Small', 'Passive', 'Terrestrial', 4, 'The best man´s friend
 print(CreateAnimal.generatingAnimal(dog))
 
 # Criando um peixe (Dourado)
-shark = CreateAnimal('Medium', 'Neutral', 'Aquatic')
+shark = CreateAnimal('Medium', 'Neutral', 'Aquatic', 'Undefined', 'Sharks do not watch very well, be carefull')
 print(CreateAnimal.generatingAnimal(shark))
 
 # criando um passaro (Tucano)
-dragon = CreateAnimal('Big', 'Agressive', 'Aerial')
+dragon = CreateAnimal('Big', 'Agressive', 'Aerial', 4, 'A terrible creature')
 print(CreateAnimal.generatingAnimal(dragon))
